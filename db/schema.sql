b59041b88191271f136dfc15cf5bf5de0e451a92
Michael Balagia <mbalagia@gmail.com>
2017-12-22 13:45:37 -0600

    created structure of files and folders. completed db files

---
+++ db/schema.sql
@@ -0,0 +1,10 @@
+create database burger_db;
+
+use burger_db;
+
+create table burgers(
+	id int NOT NULL AUTO_INCREMENT,
+	name varchar(255) NOT NULL,
+	eaten BOOLEAN DEFAULT false,
+	PRIMARY KEY (id)
+); 
