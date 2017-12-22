028b49924e6ab8e2bb8617d932985548db9a00d4
Michael Balagia <mbalagia@gmail.com>
2017-12-22 14:38:12 -0600

    finished burger model

---
+++ models/burger.js
@@ -0,0 +1,21 @@
+var orm=require("../config/orm.js");
+
+var burger={
+	selectAll: function(cb){
+		orm.selectAll("burgers",function(res){
+			cb(res);
+		});
+	},
+	insertOne: function(cols,vals,cb){
+		orm.insertOne("burgers",function(res){
+			cb(res);
+		});
+	},
+	update: function(objColVals, condition,cb){
+		orm.update("burgers",function(res){
+			cb(res);
+		});
+	}
+};
+
+module.exports=burgers;
